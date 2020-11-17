fd5cfa13d5b140501608fe36197e4af96426e90e
duyhungly <duykendo201@gmail.com>
2020-11-17 17:57:00 +0700

    Change apperance for button

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,27 +1,39 @@
 <template>
-  <div class="product">
+  <div class="product" style="margin-top:70px">
     <div class="product-bar">
-      <!-- Laptop button -->
-      <button class="laptop-button">
-        <div width="25%" data-track-content="true" data-content-region-name="hotDealTab" data-content-name="Tuần lễ Laptop">
-          <h5 >Tuần lễ Laptop</h5>
-          <div>Giá shock - giảm tới 2.3 triệu</div>
+
+        <el-col :span="12">
+          <div class="grid-content bg-purple">
+            <!-- Laptop button -->
+          <button style="width:100%" >
+            <div  data-track-content="true" data-content-region-name="hotDealTab" data-content-name="Tuần lễ Laptop">
+              <div>Tuần lễ Laptop</div>
+              <div>Giá shock - giảm tới 2.3 triệu</div>
+              </div>
+          </button>
           </div>
-      </button>
+          </el-col>
+  
+        <el-col :span="12">
+          <div class="grid-content bg-purple">
       <!-- Smart Phone Button -->
-      <button class="smartphone-button">
-        <div width="25%" data-track-content="true" data-content-region-name="hotDealTab" data-content-name="SmartPhone">
-          <h5>Điện Thoại</h5>
+      <button style="width:100%">
+        <div  data-track-content="true" data-content-region-name="hotDealTab" data-content-name="SmartPhone">
+          <div>Điện Thoại</div>
           <div>Giá shock - giảm tới 2.3 triệu</div>
         </div>
       </button>
+       </div>
+       </el-col>
     </div>
     <div style="text-align: right">
       <a href="" style="color:white">Xem tất cả ></a>
     <hr/>
     </div>
+
     <el-container>
-      <el-aside width="260px"></el-aside>
+      <el-aside width="260px">
+                  <img class="image" src="../assets/background/laptop.jpg" alt="LAPTOP - MACBOOK">      </el-aside>
       <el-main>
     <div id="product-slider">
       <section>
@@ -124,30 +136,31 @@ export default Vue.extend({
 </script>
 
 <style scoped>
-/* .container{
+.container{
   margin: 0 20px;
-} */
+}
 button:hover{
   font-weight: bold;
-  background-color: gray;
+  background-color: #00008B;
+  color: white;
 }
 button{
   cursor: pointer;
   border-radius: 0.5rem;
   display: block;
+  font-size: 20px;
 }
 hr{
   margin: 0 !important;
 }
 .product-bar{
-padding-top:30px;
 display: flex;
 justify-content: center;
 align-items: center;
 height: 60px;
 
 }
-.product-bar h3 {
+/* .product-bar h3 {
   color: white;
   display: inline;
   margin: 0 30px;
@@ -162,7 +175,7 @@ height: 60px;
   color: white;
   text-decoration: none;
   font-size: 13px;
-}
+} */
 #product-slider {
   padding-left: 50px;
 }
@@ -188,10 +201,10 @@ height: 60px;
 
 }
 .product {
-  background-image: url("../assets/background/laptop.jpg");
+  background-image: url("../assets/background/background.jpg"); 
   background-repeat: no-repeat;
   background-size: cover;
-  padding-top: 50px;
+  /* padding-top: 50px; */
 }
 .item-img {
   max-width: 170px;
